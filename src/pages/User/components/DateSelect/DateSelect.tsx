c448d69f05a395eb1d5a6851f4bad33caa1389c2
Nam Duong <phamnamduong583@gmail.com>
2024-10-14 14:16:11 +0700

    pages done

---
+++ src/pages/User/components/DateSelect/DateSelect.tsx
@@ -0,0 +1,90 @@
+import { range } from 'lodash'
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+interface Props {
+  onChange?: (value: Date) => void
+  value?: Date
+  errorMessage?: string
+}
+
+export default function DateSelect({ value, onChange, errorMessage }: Props) {
+  const { t } = useTranslation('info')
+  const [date, setDate] = useState({
+    date: value?.getDate() || 1,
+    month: value?.getMonth() || 0,
+    year: value?.getFullYear() || 1990
+  })
+
+  useEffect(() => {
+    if (value) {
+      setDate({
+        date: value.getDate(),
+        month: value.getMonth(),
+        year: value.getFullYear()
+      })
+    }
+  }, [value])
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value: valueFromSelect, name } = event.target
+    const newDate = {
+      date: value?.getDate() || date.date,
+      month: value?.getMonth() || date.month,
+      year: value?.getFullYear() || date.year,
+      [name]: Number(valueFromSelect)
+    }
+    setDate(newDate)
+    onChange && onChange(new Date(newDate.year, newDate.month, newDate.date))
+  }
+
+  return (
+    <div className='mt-2 flex flex-wrap flex-col sm:flex-row'>
+      <div className='sm:w-[20%] truncate pt-3 sm:text-right capitalize'>{t('Profile.Date of Birth')}</div>
+      <div className='sm:w-[80%] sm:pl-5'>
+        <div className='flex justify-between'>
+          <select
+            onChange={handleChange}
+            name='date'
+            className='h-10 w-[32%] rounded-sm border border-black/10 px-3 hover:border-orange cursor-pointer'
+            value={value?.getDate() || date.date}
+          >
+            <option disabled>Ngày</option>
+            {range(1, 32).map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+          <select
+            onChange={handleChange}
+            name='month'
+            className='h-10 w-[32%] rounded-sm border border-black/10 px-3 hover:border-orange cursor-pointer'
+            value={value?.getMonth() || date.month}
+          >
+            <option disabled>Tháng</option>
+            {range(0, 12).map((item) => (
+              <option value={item} key={item}>
+                {item + 1}
+              </option>
+            ))}
+          </select>
+          <select
+            onChange={handleChange}
+            name='year'
+            className='h-10 w-[32%] rounded-sm border border-black/10 px-3 hover:border-orange cursor-pointer'
+            value={value?.getFullYear() || date.year}
+          >
+            <option disabled>Năm</option>
+            {range(1990, 2025).map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errorMessage}</div>
+      </div>
+    </div>
+  )
+}
