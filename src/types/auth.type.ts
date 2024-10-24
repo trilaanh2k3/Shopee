1b1a1630a4423f65e887d7acf1d069d91e83ab50
Nam Duong <phamnamduong583@gmail.com>
2024-10-24 21:36:23 +0700

    build test

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,6 +1,6 @@
 import { User } from './user.type'
 import { SuccessResponse } from './utils.type'
-
+//
 export type AuthResponse = SuccessResponse<{
   access_token: string
   refresh_token: string
